cba9c80c65d5a5afa8bd75f26f5c25b2563c6814
Navfront <b3rt@yandex.ru>
2022-02-07 23:09:27 +0300

    styling form

--- source/js/site-navigation.js
+++ source/js/site-navigation.js
@@ -1,8 +1,10 @@
-const burger = document.querySelector(".button-burger");
+const navigation = document.querySelector(".site-navigation");
+const burger = navigation.querySelector(".button-burger");
 const burgerIcon = burger.querySelector(".button-burger__icon");
-const menu = document.querySelector(".site-navigation__list");
+const menu = navigation.querySelector(".site-navigation__list");
 
 burger.addEventListener("click", () => {
   burgerIcon.classList.toggle("button-burger__icon--close");
   menu.classList.toggle("site-navigation__list--active");
+  navigation.classList.toggle("site-navigation--closed");
 });
