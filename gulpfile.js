5426d3278ec807a329b92ff574cc3352ede27715
Navfront <b3rt@yandex.ru>
2022-10-01 16:31:10 +0300

    Настроил деплой билда

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,15 @@ const webp = require("gulp-webp");
 const imagemin = require("gulp-imagemin");
 const svgstore = require("gulp-svgstore");
 const fileChanged = require("gulp-changed");
+const ghPages = require('gh-pages');
+const path = require('path');
+
+// Deploy
+
+function deploy(cb) {
+  ghPages.publish(path.join(process.cwd(), './build'), cb);
+}
+exports.deploy = deploy;
 
 // Styles
 
